bcad551f3f9146956fe2d6fb5242d7da298fce9b
Arkadius Schuchhardt <arkadius@schuchhardt.com>
2025-01-18 23:56:40 +0100

    fix: remove test task

--- murkelhausen_app_v2/tasks/__init__.py
+++ murkelhausen_app_v2/tasks/__init__.py
@@ -7,7 +7,6 @@ from murkelhausen_app_v2.config import config
 from murkelhausen_app_v2.tasks.buergeramt import (
     get_next_free_appointment_from_buergeramt,
 )
-from murkelhausen_app_v2.tasks.test import test_task
 
 log = logging.getLogger(__name__)
 scheduler = BackgroundScheduler()
@@ -17,8 +16,5 @@ if config.tasks.buergeramt_task.active:
     trigger = IntervalTrigger(minutes=config.tasks.buergeramt_task.schedule_minutes)
     scheduler.add_job(get_next_free_appointment_from_buergeramt, trigger)
     log.info("Buergeramt task added")
-
-trigger = IntervalTrigger(minutes=1)
-scheduler.add_job(test_task, trigger)
 
 __all__ = ["scheduler"]
